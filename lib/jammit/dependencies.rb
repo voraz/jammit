ddbb7660ba922a561914999c78b2880f74b814f9
Sam Granieri <sam@samgranieri.com>
2010-01-19 22:22:35 -0600

    hiding rails from the command line for now

--- lib/jammit/dependencies.rb
+++ lib/jammit/dependencies.rb
@@ -5,25 +5,25 @@ require 'base64'
 require 'pathname'
 require 'fileutils'
 
-# Gem Dependencies:
+unless defined? Rails
+  require 'rubygems'
+end
 require 'yui/compressor'
 require 'closure-compiler'
-# require 'active_support'
+require 'active_support'
 
 # Load initial configuration before the rest of Jammit.
-Jammit.load_configuration(Jammit::DEFAULT_CONFIG_PATH) if defined?(Rails)
 
 # Jammit Core:
 require 'jammit/compressor'
 require 'jammit/packager'
 # Jammit Rails Integration:
-# if defined?(Rails)
-  # require 'jammit/controller' # Rails will auto-load 'jammit/helper' for us.
-  # require 'jammit/routes'
-# end
-ActionView::Base.send(:include, JammitHelper)
-if Rails.env.development?
-  ActionController::Base.class_eval do
-    append_before_filter { Jammit.reload! }
+if defined?(Rails)
+  Jammit.load_configuration(Jammit::DEFAULT_CONFIG_PATH) if defined?(Rails)
+  ActionView::Base.send(:include, JammitHelper)
+  if Rails.env.development?
+    ActionController::Base.class_eval do
+      append_before_filter { Jammit.reload! }
+    end
   end
 end
